bb40ed26468bd8b4f0922551f6d1cffd7e17969d
christian telemaque <salvator8@hotmail.com>
2014-09-25 09:39:31 -0400

    -create calculerTailleX() - Fill out the grille tab

--- inf4230_tp1/src/sokoban/Probleme.java
+++ inf4230_tp1/src/sokoban/Probleme.java
@@ -8,14 +8,37 @@ package sokoban;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.ArrayList;
-
+  
 /**
  *  Représente un problème chargé d'un fichier test sokoban??.txt.
  */
 public class Probleme {
+
+    private static int calculerTailleY(ArrayList<String> lignes) {
+ 
+    ArrayList<Integer> plusGrandTaille = new ArrayList<>();
+    
+    for(int i = 0; i < lignes.size(); i++)
+    {
+        if(plusGrandTaille.isEmpty())plusGrandTaille.add(lignes.get(i).length());
+        else {
+            for(int u = 0 ; u <  plusGrandTaille.size(); u++){
+            if((lignes.get(i).length())< (plusGrandTaille.get(u))){
+             plusGrandTaille.add(u, lignes.get(i).length());
+            }
+        }
+            
+        if((lignes.get(i).length()) > (plusGrandTaille.get(plusGrandTaille.size())))
+            plusGrandTaille.add((lignes.get(i).length()));
+            
+        }
+    }
+    return plusGrandTaille.get(plusGrandTaille.size());
+    }
     public Grille grille;
     public EtatSokoban etatInitial;
     public But but;
+    
     
     private Probleme(){
     }
@@ -24,7 +47,9 @@ public class Probleme {
         // Lire les lignes dans fichiers
         ArrayList<String> lignes = new ArrayList<String>();
         String ligne;
-        while((ligne = br.readLine())!=null && !ligne.isEmpty()){
+        
+
+         while((ligne = br.readLine())!=null && !ligne.isEmpty()){
             lignes.add(ligne);
         }
         
@@ -43,6 +68,17 @@ public class Probleme {
         
         // Certains grilles pourraient contenir des astérisques '*' et plus '+'. 
         // Ces symboles peuvent être ignorés et traités comme des espaces ' '.
+        
+        int tailleX = calculerTailleY(lignes);
+        char [][] grille = new char [tailleX][lignes.size()];
+        
+        for(int y = 0; y < lignes.size(); y++){
+            int tailleLigne = lignes.get(y).length();
+            for(int x = 0; x < tailleLigne; x++){
+                
+                grille[x][y] = lignes.get(y).charAt(x);
+            }
+        }
         
         return probleme;
     }
