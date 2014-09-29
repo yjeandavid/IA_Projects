893546554170e4ea3c474c51c62d032cc66754ec
Michael <Michael@192.168.2.17>
2014-09-29 09:56:59 -0400

    correction d'erreurs d'algorithme

--- inf4230_tp1/src/sokoban/Grille.java
+++ inf4230_tp1/src/sokoban/Grille.java
@@ -34,21 +34,37 @@ public class Grille implements astar.Monde, astar.But {
         EtatSokoban etat = (EtatSokoban) e;
         ArrayList<astar.Action> actions = new ArrayList<>();
         Point bonhomme = etat.getBonhomme();
+        ArrayList<Point> blocs = (ArrayList<Point>) etat.getBlocs();
         
-        if (bonhomme.x > 0) {
-            if (grille.get(new Point(bonhomme.x-1, bonhomme.y)) != null) {
-                actions.add(new ActionDeplacement("W"));
-            } else if (grille.get(new Point(bonhomme.x+1, bonhomme.y)) != null) {
+        if (bonhomme.x >= 1 && bonhomme.y >= 1) {
+            if (grille.get(new Point(bonhomme.x+1,bonhomme.y)) == null && !blocs.contains(new Point(bonhomme.x+1,bonhomme.y))) {
+                actions.add(new ActionDeplacement("E"));
+            } else if (grille.get(new Point(bonhomme.x+1,bonhomme.y)) == null && blocs.contains(new Point(bonhomme.x+1,bonhomme.y))
+                && grille.get(new Point(bonhomme.x+2,bonhomme.y)) == null && !blocs.contains(new Point(bonhomme.x+2,bonhomme.y))) {
                 actions.add(new ActionDeplacement("E"));
             }
-        } else if (bonhomme.y > 0) {
-            if (grille.get(new Point(bonhomme.x, bonhomme.y+1)) != null) {
+            
+            if (grille.get(new Point(bonhomme.x-1,bonhomme.y)) == null && !blocs.contains(new Point(bonhomme.x-1,bonhomme.y))) {
+                actions.add(new ActionDeplacement("W"));
+            } else if (grille.get(new Point(bonhomme.x-1,bonhomme.y)) == null && blocs.contains(new Point(bonhomme.x-1,bonhomme.y))
+                && grille.get(new Point(bonhomme.x-2,1)) == null && !blocs.contains(new Point(bonhomme.x-2,1))) {
+                actions.add(new ActionDeplacement("W"));
+            }
+            
+            if (grille.get(new Point(bonhomme.x,bonhomme.y+1)) == null && !blocs.contains(new Point(bonhomme.x,bonhomme.y+1))) {
                 actions.add(new ActionDeplacement("S"));
-            } else if (grille.get(new Point(bonhomme.x, bonhomme.y-1)) != null) {
+            } else if (grille.get(new Point(bonhomme.x,bonhomme.y+1)) == null && blocs.contains(new Point(bonhomme.x,bonhomme.y+1))
+                    && grille.get(new Point(bonhomme.x,bonhomme.y+2)) == null && !blocs.contains(new Point(bonhomme.x,bonhomme.y+2))) {
+                actions.add(new ActionDeplacement("S"));
+            }
+            
+            if (grille.get(new Point(bonhomme.x,bonhomme.y-1)) == null && !blocs.contains(new Point(bonhomme.x,bonhomme.y-1))) {
+                actions.add(new ActionDeplacement("N"));
+            } else if (grille.get(new Point(bonhomme.x,bonhomme.y-1)) == null && blocs.contains(new Point(bonhomme.x,bonhomme.y-1))
+                    && grille.get(new Point(bonhomme.x,bonhomme.y-2)) == null && !blocs.contains(new Point(bonhomme.x,bonhomme.y-2))) {
                 actions.add(new ActionDeplacement("N"));
             }
         }
-        
         
         return actions;
     }
